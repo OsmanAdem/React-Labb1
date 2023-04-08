e557121d1fda81a24fb1ee3a39fba76ceae56083
Osman Adem <112473033+OsmanAdem@users.noreply.github.com>
2023-04-08 02:32:58 +0200

    First commit

---
+++ my-app1/src/components/AboutPage/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>
+                We are a cozy coffee shop located in the heart of the city,
+                serving up delicious coffee, pastries, and snacks.
+            </p>
+            <p>
+                Our mission is to create a welcoming environment where everyone
+                can come to relax, recharge, and enjoy a quality cup of coffee.
+            </p>
+            <h2>Our History</h2>
+            <p>
+                Our coffee shop was founded in 2010 by two friends who shared a
+                passion for coffee and a desire to create a community gathering
+                place.
+            </p>
+            <p>
+                Over the years, we have grown and expanded our menu to include a
+                variety of beverages and food options, but our commitment to
+                quality and community remains the same.
+            </p>
+            <h2>Our Team</h2>
+            <p>
+                We are a team of dedicated coffee enthusiasts who love what we
+                do. From our baristas to our bakers, we take pride in every cup
+                of coffee and every pastry we serve.
+            </p>
+            <p>Stop by and say hello - we would love to meet you!</p>
+        </div>
+    )
+}
+
+export default About
