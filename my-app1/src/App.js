fede143f7865b6fd5291b9b419ed861a3c66b77c
Osman Adem <112473033+OsmanAdem@users.noreply.github.com>
2023-04-08 04:02:00 +0200

    First commit

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -2,11 +2,7 @@ import LandingPage from './components/LandingPage'
 import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            <LandingPage />
-        </div>
-    )
+    return <div className="App"></div>
 }
 
 export default App
